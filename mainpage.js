2d6fefe9f50e6e9dcc9ffbfd282e0397161bb5ab
Aleynaozc <aleynaozcelik48@gmail.com>
2022-05-18 12:38:48 +0300

    add-modal-animation

--- mainpage.js
+++ mainpage.js
@@ -1,6 +1,69 @@
+const openModalButton=document.querySelector('#userIcon');
+const signInModalButton=document.querySelector('#signIn');
+const signUpModalButton=document.querySelector('#signUp');
+const signInSignUpModal=document.querySelector('#signInSignUpModal');
+const signInOpenModal=document.querySelector('#signInModal');
+const signUpOpenModal=document.querySelector('#signUpModal');
+const signInSignUpButton=document.querySelector('.signIn_signUp-form');
+const signInButton=document.querySelector('#signinbutton');
+const signUpButton=document.querySelector('#signupbutton');
+const backPage=document.getElementById('backPage')
 
-const openModalButton=document.getElementById('#SignIn') ;
-openModalButton.addEventListener('click', openModal);
+
+
+//OPEN SIGN IN AND SIGN UP MODAL
 const openModal =()=>{
-   quiz
+    signInSignUpModal.classList.toggle("show");
+    signInSignUpModal.classList.toggle('height-animation');
+    signInButton.style.display='block';
+    signUpButton.style.display='block';
+    backPage.style.display='block';
+    
 }
+openModalButton.addEventListener('click', openModal);
+
+//BUTTON DISABLED
+const buttonNone=()=>{
+    signInSignUpButton.style.display="none";
+ }
+//OPEN SIGN IN MODAL
+const OpenSignInModal=()=>{
+    signInOpenModal.classList.add("show");
+} 
+const SignInanimationOfButton =()=>{
+    signInButton.classList.add('button-animation');
+    signUpButton.classList.add('button-animation');
+    setTimeout(buttonNone,600);
+    setTimeout(OpenSignInModal,300);
+   
+}
+signInModalButton.addEventListener('click', SignInanimationOfButton);
+
+
+
+//OPEN SIGN UP MODAL
+const OpenSignUpModal=()=>{
+    signUpOpenModal.classList.add("show");
+}
+const SignUpanimationOfButton =()=>{
+    signInButton.classList.add('button-animation');
+    signUpButton.classList.add('button-animation');
+    setTimeout(buttonNone,600);
+    setTimeout(OpenSignUpModal,550);
+}
+signUpModalButton.addEventListener('click', SignUpanimationOfButton);
+
+const backPageButton =()=>{
+    signInSignUpModal.classList.remove('height-animation');
+        signInSignUpModal.classList.add('closeModal-animation');
+        signInButton.style.display='none';
+        signUpButton.style.display='none';
+        backPage.style.display='none';
+        setTimeout(removeModal,700);
+
+}
+const removeModal=()=>{
+    signInSignUpModal.classList.remove('show');
+    
+}
+backPage.addEventListener('click',backPageButton);
